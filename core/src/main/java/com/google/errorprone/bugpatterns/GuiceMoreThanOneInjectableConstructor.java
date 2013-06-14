419c2562eca3ab9280ba080b741ff04a82a368c3
Steven Goldfeder <sgoldfed@gmail.com>
2013-06-14 16:01:37 -0700

    c

--- core/src/main/java/com/google/errorprone/bugpatterns/GuiceMoreThanOneInjectableConstructor.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/GuiceMoreThanOneInjectableConstructor.java
@@ -21,7 +21,6 @@ import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.Tree;
 
-import java.util.List;
 
 /**
  * Matches if a class has to constructors annotated with @Inject
@@ -30,65 +29,58 @@ import java.util.List;
  */
 @BugPattern(name = "GuiceMoreThanOneInjectableConstructor",
 summary = "A class may not have more than one injectable constructor.",
-explanation ="",
+explanation = "Having more than one injectable constructor will throw a runtime error in Guice",
 category = GUICE, severity = ERROR, maturity = EXPERIMENTAL)
-public class GuiceMoreThanOneInjectableConstructor extends DescribingMatcher<ClassTree>  {
+public class GuiceMoreThanOneInjectableConstructor extends DescribingMatcher<MethodTree>  {
   
   private static final String GUICE_INJECT_ANNOTATION = "com.google.inject.Inject";
   private static final String JAVAX_INJECT_ANNOTATION = "javax.inject.Inject";
-  
-  
-    @SuppressWarnings("unchecked")
-  Matcher<MethodTree> injectOnMethodMatcher = Matchers.<MethodTree>anyOf(hasAnnotation(GUICE_INJECT_ANNOTATION),
+
+
+  @SuppressWarnings("unchecked")
+  Matcher<MethodTree> injectOnMethodMatcher = Matchers.<MethodTree>anyOf(
+      hasAnnotation(GUICE_INJECT_ANNOTATION),
       hasAnnotation(JAVAX_INJECT_ANNOTATION));
 
-
   @Override
-  public boolean matches(ClassTree classTree, VisitorState state) {
+  public boolean matches(MethodTree methodTree, VisitorState state) {
     int numberOfInjectableConstructors = 0;
-    for (Tree member : classTree.getMembers()) {
-      if (member instanceof MethodTree && ASTHelpers.getSymbol(member).isConstructor() 
-          && injectOnMethodMatcher.matches((MethodTree) member, state)){
-        numberOfInjectableConstructors++;
+    if (ASTHelpers.getSymbol(methodTree).isConstructor()
+        && injectOnMethodMatcher.matches(methodTree, state)){
+      for (Tree member : ((ClassTree) state.getPath().getParentPath().getLeaf()).getMembers()) {
+        if (member instanceof MethodTree && ASTHelpers.getSymbol(member).isConstructor() 
+            && injectOnMethodMatcher.matches((MethodTree) member, state)){
+          numberOfInjectableConstructors++;
+        }
       }
     }
-    if(numberOfInjectableConstructors > 1){
-      return true;
-    }
-    return false;
+   return (numberOfInjectableConstructors > 1) ? true : false;
   }
 
   @SuppressWarnings("unchecked")
   @Override
-  public Description describe(ClassTree classTree, VisitorState state) {
-    int injectedConstructor = 0;
-    for (Tree member : classTree.getMembers()) {
-      if (member instanceof MethodTree && ASTHelpers.getSymbol(member).isConstructor()
-          && injectOnMethodMatcher.matches((MethodTree) member, state)){
-        injectedConstructor++; 
-        if(injectedConstructor > 1){
-        //puts error on second injectable constructor
-          MethodTree constructor = (MethodTree)member;
-          List<? extends AnnotationTree> annotationsOnConstructor =constructor.getModifiers().getAnnotations();
-          for(AnnotationTree annotation: annotationsOnConstructor){
-            if(ASTHelpers.getSymbol(annotation).equals(state.getSymbolFromString(GUICE_INJECT_ANNOTATION)) 
-                || ASTHelpers.getSymbol(annotation).equals(state.getSymbolFromString(JAVAX_INJECT_ANNOTATION)) ){
-              return new Description(annotation,getDiagnosticMessage(),new SuggestedFix().delete(annotation));
+  public Description describe(MethodTree methodTree, VisitorState state) {
+          for (AnnotationTree annotation : methodTree.getModifiers().getAnnotations()){
+            if (ASTHelpers.getSymbol(annotation).equals
+                  (state.getSymbolFromString(GUICE_INJECT_ANNOTATION)) 
+                || ASTHelpers.getSymbol(annotation).equals
+                    (state.getSymbolFromString(JAVAX_INJECT_ANNOTATION))){
+              return new Description(
+                  annotation, getDiagnosticMessage(), new SuggestedFix().delete(annotation));
             }
           }
-        }          
-      }
-    }
-    throw new IllegalStateException("Expected to find more than once constructor annotated with @Inject");
+    throw new IllegalStateException(
+        "Expected to find more than once constructor annotated with @Inject");
   }
 
   public static class Scanner extends com.google.errorprone.Scanner {
-    public DescribingMatcher<ClassTree> classMatcher = new GuiceMoreThanOneInjectableConstructor();
+    public DescribingMatcher<MethodTree> methodMatcher = 
+        new GuiceMoreThanOneInjectableConstructor();
 
     @Override
-    public Void visitClass(ClassTree classTree, VisitorState visitorState) {
-      evaluateMatch(classTree, visitorState, classMatcher);
-      return super.visitClass(classTree, visitorState);
+    public Void visitMethod(MethodTree methodTree, VisitorState visitorState) {
+      evaluateMatch(methodTree, visitorState, methodMatcher);
+      return super.visitMethod(methodTree, visitorState);
     }
   }
 }
