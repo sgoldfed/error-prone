6904609ebb55d9f9956c102e570c4f357cfcd40e
Steven Goldfeder <sgoldfed@gmail.com>
2013-06-14 16:01:14 -0700

    c

--- core/src/test/resources/com/google/errorprone/bugpatterns/GuiceMoreThanOneInjectableConstructorPositiveCases.java
+++ core/src/test/resources/com/google/errorprone/bugpatterns/GuiceMoreThanOneInjectableConstructorPositiveCases.java
@@ -9,32 +9,37 @@ import java.beans.ConstructorProperties;
 
 public class GuiceMoreThanOneInjectableConstructorPositiveCases {
 
+  /**
+   * class has 2 constructors, both are injectable
+   */
   public class TestClass1 {
-    @Inject
-    public TestClass1() {
-    }
     //BUG: Suggestion includes "remove"
     @Inject
-    public TestClass1(int n) {
-    }
+    public TestClass1() {}
+    //BUG: Suggestion includes "remove"
+    @Inject
+    public TestClass1(int n) {}
   }
 
+  /**
+   * class has 3 constructors, 2 are injectable
+   */
   public class TestClass2 {
-    @Inject 
-    public TestClass2() {
-    }
     //BUG: Suggestion includes "remove"
     @Inject 
-    public TestClass2(int m, int n) {
-    }
-    public TestClass2(int n,String s) {
-    }
+    public TestClass2() {}
+    //BUG: Suggestion includes "remove"
+    @Inject 
+    public TestClass2(int m, int n) {}
+    public TestClass2(int n, String s) {}
   }
 
-  //testing that the error appears on the @Inject annotation even in the presence 
-  //of other annotations
+  /**testing that the error appears on the @Inject annotation even in the presence 
+   * of other annotations
+   */
   public class TestClass3 {
-    private int m,n;
+    private int m, n;
+    //BUG: Suggestion includes "remove"
     @Inject
     public TestClass3(){
       m = 0;
@@ -42,7 +47,7 @@ public class GuiceMoreThanOneInjectableConstructorPositiveCases {
     }
     //BUG: Suggestion includes "remove"
     @Inject 
-    @ConstructorProperties({"m","n"})
+    @ConstructorProperties({"m", "n"})
     public TestClass3(int m, int n) {
       this.m = m;
       this.n = n;
@@ -56,16 +61,19 @@ public class GuiceMoreThanOneInjectableConstructorPositiveCases {
 
   }
 
-  //testing that the error appears on the @Inject annotation even in the presence 
-  //of other annotations
+  /**
+   * testing that the error appears on the @Inject annotation even in the presence 
+   * of other annotations
+   */
   public class TestClass4 {
-    private int m,n;
+    private int m, n;
+    //BUG: Suggestion includes "remove"
     @Inject
     public TestClass4(){
       m = 0;
       n = 0;
     }
-    @ConstructorProperties({"m","n"})
+    @ConstructorProperties({"m", "n"})
     //BUG: Suggestion includes "remove"
     @Inject
     public TestClass4(int m, int n) {
