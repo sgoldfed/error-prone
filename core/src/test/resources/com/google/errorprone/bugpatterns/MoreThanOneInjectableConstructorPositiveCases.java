ced125f797bcffa6c58f024302a527e62da14c12
Steven Goldfeder <sgoldfed@gmail.com>
2013-06-17 18:10:23 -0700

    base

--- core/src/test/resources/com/google/errorprone/bugpatterns/MoreThanOneInjectableConstructorPositiveCases.java
+++ core/src/test/resources/com/google/errorprone/bugpatterns/MoreThanOneInjectableConstructorPositiveCases.java
@@ -7,7 +7,7 @@ import java.beans.ConstructorProperties;
  * @author sgoldfeder@google.com(Steven Goldfeder)
  */
 
-public class GuiceMoreThanOneInjectableConstructorPositiveCases {
+public class MoreThanOneInjectableConstructorPositiveCases {
 
   /**
    * class has 2 constructors, both are injectable
@@ -30,61 +30,39 @@ public class GuiceMoreThanOneInjectableConstructorPositiveCases {
     public TestClass2() {}
     //BUG: Suggestion includes "remove"
     @Inject 
-    public TestClass2(int m, int n) {}
-    public TestClass2(int n, String s) {}
+    public TestClass2(int n) {}
+    public TestClass2(String s) {}
   }
 
-  /**testing that the error appears on the @Inject annotation even in the presence 
-   * of other annotations
+  /**
+   * testing that the error appears on the @Inject annotation even in the presence of other
+   * annotations
    */
   public class TestClass3 {
-    private int m, n;
     //BUG: Suggestion includes "remove"
     @Inject
     public TestClass3(){
-      m = 0;
-      n = 0;
     }
     //BUG: Suggestion includes "remove"
     @Inject 
     @ConstructorProperties({"m", "n"})
     public TestClass3(int m, int n) {
-      this.m = m;
-      this.n = n;
     }
-    public int getM() {
-      return m;
-    }
-    public int getN() {
-      return n;
-    }
-
-  }
+   }
 
   /**
    * testing that the error appears on the @Inject annotation even in the presence 
    * of other annotations
    */
   public class TestClass4 {
-    private int m, n;
     //BUG: Suggestion includes "remove"
     @Inject
     public TestClass4(){
-      m = 0;
-      n = 0;
     }
     @ConstructorProperties({"m", "n"})
     //BUG: Suggestion includes "remove"
     @Inject
     public TestClass4(int m, int n) {
-      this.m = m;
-      this.n = n;
-    }
-    public int getM() {
-      return m;
-    }
-    public int getN() {
-      return n;
     }
   }
 }
