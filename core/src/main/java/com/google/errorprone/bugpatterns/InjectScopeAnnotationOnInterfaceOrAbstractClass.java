502d51ac910eaebd68cd8e284e69534433946022
Steven Goldfeder <sgoldfed@gmail.com>
2013-06-20 10:16:19 -0700

    new branch for scope on abstract type

--- core/src/main/java/com/google/errorprone/bugpatterns/InjectScopeAnnotationOnInterfaceOrAbstractClass.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/InjectScopeAnnotationOnInterfaceOrAbstractClass.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.errorprone.bugpatterns;
 
 import static com.google.errorprone.BugPattern.Category.INJECT;
 import static com.google.errorprone.BugPattern.MaturityLevel.EXPERIMENTAL;
 import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
 import static com.google.errorprone.matchers.Matchers.hasAnnotation;
-import static com.sun.source.tree.Tree.Kind.INTERFACE;
+
 import static javax.lang.model.element.Modifier.ABSTRACT;
 
 import com.google.errorprone.BugPattern;
@@ -14,13 +30,12 @@ import com.google.errorprone.matchers.DescribingMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.util.ASTHelpers;
 
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.Tree;
-
-
-
+import com.sun.tools.javac.code.Flags;
 /**
  * @author sgoldfeder@google.com (Steven Goldfeder)
  */
@@ -47,7 +62,7 @@ public class InjectScopeAnnotationOnInterfaceOrAbstractClass
     @Override
     public boolean matches(ClassTree classTree, VisitorState state) {
       return classTree.getModifiers().getFlags().contains(ABSTRACT)
-          || classTree.getKind().equals(INTERFACE);
+          || (ASTHelpers.getSymbol(classTree).flags() & Flags.INTERFACE) != 0;
     }
   };
 
