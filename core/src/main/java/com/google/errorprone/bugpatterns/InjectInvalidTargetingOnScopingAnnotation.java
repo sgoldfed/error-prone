4dee2999bcbe40e1e036d3a9f901cd694edcfc87
Steven Goldfeder <sgoldfed@gmail.com>
2013-06-20 18:51:38 -0700

    new bugpattern to check that scoping annotations have @Target set to TYPE and/or METHOD. I still need to discuss the appropriate SuggestedFix() for the various cases.

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/InjectInvalidTargetingOnScopingAnnotation.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.Category.INJECT;
+import static com.google.errorprone.BugPattern.MaturityLevel.EXPERIMENTAL;
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+import static com.google.errorprone.matchers.Matchers.hasAnnotation;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.DescribingMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.util.ASTHelpers;
+
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.tools.javac.code.Flags;
+import com.sun.tools.javac.model.JavacElements;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * @author sgoldfeder@google.com (Steven Goldfeder)
+ */
+@BugPattern(name = "InjectInvalidTargetingOnScopingAnnotation",
+    summary = "The @Target of a scoping annotation must be set to METHOD and/or TYPE",
+    explanation = "", category = INJECT, severity = ERROR, maturity = EXPERIMENTAL)
+public class InjectInvalidTargetingOnScopingAnnotation extends DescribingMatcher<ClassTree> {
+
+  private static final String GUICE_SCOPE_ANNOTATION = "com.google.inject.ScopeAnnotation";
+  private static final String JAVAX_SCOPE_ANNOTATION = "javax.inject.Scope";
+
+  /**
+   * Matches classes that are annotated with @Scope or @ScopeAnnotation.
+   */
+  @SuppressWarnings("unchecked")
+  private Matcher<ClassTree> scopeAnnotationMatcher = Matchers.<ClassTree>anyOf(
+      hasAnnotation(GUICE_SCOPE_ANNOTATION), hasAnnotation(JAVAX_SCOPE_ANNOTATION));
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public final boolean matches(ClassTree classTree, VisitorState state) {
+    if ((ASTHelpers.getSymbol(classTree).flags() & Flags.ANNOTATION) != 0
+        && scopeAnnotationMatcher.matches(classTree, state)) {
+      Target target = JavacElements.getAnnotation(ASTHelpers.getSymbol(classTree), Target.class);
+      boolean hasTypeOrMethodTargeting = false;
+      if (target != null) {
+        for (ElementType elementType : target.value()) {
+          if (elementType != METHOD && elementType != TYPE) {
+            return true;
+          } else if (elementType == METHOD || elementType == TYPE) {
+            hasTypeOrMethodTargeting = true;
+          }
+        }
+        // this will return true if the target of a scoping annotation is specified as @Target({})
+        return !hasTypeOrMethodTargeting;
+      }
+      // no target specified
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public Description describe(ClassTree classTree, VisitorState state) {
+    AnnotationTree scope = null;
+    for (AnnotationTree annotationTree : classTree.getModifiers().getAnnotations()) {
+      if (ASTHelpers.getSymbol(annotationTree)
+          .equals(state.getSymbolFromString(JAVAX_SCOPE_ANNOTATION)) || ASTHelpers.getSymbol(
+          annotationTree).equals(state.getSymbolFromString(GUICE_SCOPE_ANNOTATION))) {
+        scope = annotationTree;
+      }
+
+    }
+    return new Description(classTree, getDiagnosticMessage(), new SuggestedFix().delete(scope));
+  }
+
+  public static class Scanner extends com.google.errorprone.Scanner {
+    public DescribingMatcher<ClassTree> classMatcher =
+        new InjectInvalidTargetingOnScopingAnnotation();
+
+    @Override
+    public Void visitClass(ClassTree classTree, VisitorState visitorState) {
+      evaluateMatch(classTree, visitorState, classMatcher);
+      return super.visitClass(classTree, visitorState);
+    }
+  }
+}
